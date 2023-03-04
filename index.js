67d5200ab4374e01f76e723d0b02f196f9353075
Dhia' Alhaq Shalabi <dhia.shalabi@gmail.com>
2023-03-04 19:50:17 +0300

    Completed assignment.

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 // code your solution here
+
+function saturdayFun(activity = 'roller-skate') {
+    return `This Saturday, I want to ${activity}!`;
+}
+
+function mondayWork(activity = 'go to the office') {
+    return `This Monday, I will ${activity}.`;
+}
+
+function wrapAdjective(flair = '*') {
+    return function (adjective = 'special') {
+        return `You are ${flair}${adjective}${flair}!`;
+    }
+}
+
+const encouragingPromptFunction = wrapAdjective('!!!');
+console.log(encouragingPromptFunction('a dedicated programmer'));
+console.log(saturdayFun());
+console.log(saturdayFun('dance'));
+
+console.log(mondayWork());
+console.log(mondayWork('work from home'));
+
+console.log(encouragingPromptFunction('an awesome person'));
+console.log(wrapAdjective('%')('a dedicated programmer'));
